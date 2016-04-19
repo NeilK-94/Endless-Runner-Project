9b74436718eace0faee175289346189d029e9f8d
NeilK-94 <neil.kyne@gmail.com>
2016-04-19 14:25:38 +0100

    Camera now follows player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController thePlayer;
+
+    private Vector3 lastPlayerPosition; //  used to find player position
+    private float distanceToMove;   //  how much to move camera
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();   //  keep camera on player
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x; //  distance to move = current position - last position
+
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);    //  distance camera has to move 
+
+        lastPlayerPosition = thePlayer.transform.position;  //  last player position is now current position
+	}
+}
