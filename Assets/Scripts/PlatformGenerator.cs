c31cfbbbd6e702e974fa4fde532835bfe6182059
NeilK-94 <neil.kyne@gmail.com>
2016-04-19 15:21:05 +0100

    Generating scene
    Now I've gotten the level being generated as the player moves along. I
    still need to destroy the ground objects as they go behing the player
    though. Also need to add it so the new generated grounds can vary in
    height and length from player

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;   //  get width of platform
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < generationPoint.position.x)  //  if where we are right nw is more left than generation point
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+            Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+	}
+}
