6a61f3d5e087ae370b62321882ce01b28a53a445
NeilK-94 <neil.kyne@gmail.com>
2016-04-21 17:41:26 +0100

    commit

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -9,8 +9,12 @@ public class PlatformGenerator : MonoBehaviour {
 
     private float platformWidth;
 
-	// Use this for initialization
-	void Start () {
+    public float distanceBetweenhMin;
+    public float distanceBetweenMax;
+
+
+    // Use this for initialization
+    void Start () {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;   //  get width of platform
 	}
 	
@@ -18,6 +22,7 @@ public class PlatformGenerator : MonoBehaviour {
 	void Update () {
         if (transform.position.x < generationPoint.position.x)  //  if where we are right nw is more left than generation point
         {
+            distanceBetween = Random.Range(distanceBetweenhMin, distanceBetweenMax);
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
             Instantiate(thePlatform, transform.position, transform.rotation);
