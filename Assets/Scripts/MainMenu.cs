60f6703690557358a93960efa15c7d0b7dd7d055
NeilK-94 <neil.kyne@gmail.com>
2016-04-22 20:39:48 +0100

    Main Menu added with start game and quit button
    will probably add a quick pause button and a death menu as well

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{ 
+    public string startGameLevel;
+
+    public void StartGame() //  tell app to load up endless runner scene
+    {
+        Application.LoadLevel(startGameLevel);
+    }
+
+    public void QuitGame()  //  quit app
+    {
+        Application.Quit();
+
+    }
+
+}
