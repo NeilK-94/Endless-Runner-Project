e090782ed8a91a9e25a89c7e7ec16a8cf17f780c
NeilK-94 <neil.kyne@gmail.com>
2016-08-30 15:26:15 +0100

    fin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -55,27 +55,5 @@ public class GameManager : MonoBehaviour {
         theScoreManager.scoreCount = 0; //  set scre back to 0
         theScoreManager.scoreIncreasing = true;    //  take score again
     }
-/*
-    public IEnumerator RestartCoroutine()   //  coroutine runs independantly to rest of script. Allows for time delays (for delay after death)
-    {
-        theScoreManager.scoreIncreasing = false;    //  score stop increasing when dead
-        thePlayer.gameObject.SetActive(false);  //  player object becomes inactive once dead
-        yield return new WaitForSeconds(1.0f);  //  wait for one second
 
-        platformList = FindObjectsOfType<PlatformDestroyer>();  //  array of platformlist (all existing after death)
-        for (int i = 0; i < platformList.Length; i++)   //  gets all platform in platform list
-        {
-            platformList[i].gameObject.SetActive(false);    //  sets platforms to false (inactive) one by one to remove them
-        }
-
-        thePlayer.transform.position = playerStartPoint;    //  basically opposite to what we said above
-        platformGenerator.position = platformStartPoint;
-
-        thePlayer.gameObject.SetActive(true);   //  activate him again
-
-        theScoreManager.scoreCount = 0; //  set scre back to 0
-        theScoreManager.scoreIncreasing = true;    //  take score again
-    }
-
- */
 }
